9f2c63c01af74e16584de4a3bf6790522c0a9580
iva9928 <iva.hadgieva9928@gmail.com>
2025-04-18 22:18:25 +0300

    AllClasesDone

---
+++ BoardGamesCatalog/Data/Models/Boardgame.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamesCatalog.Data.Models
+{
+    public class Boardgame
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Required]
+        public int YearPublished { get; set; }
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+
+        [Required]
+        public int PublisherId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(PublisherId))]
+        public Publisher Publisher { get; set; }
+
+        [Required]
+        public int PlayerRangeId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(PlayerRangeId))]
+        public PlayerRange PlayerRange { get; set; }
+
+    }
+}
