130fb51a60f1e987cfee037429f2c57d7f13ff2f
iva9928 <iva.hadgieva9928@gmail.com>
2025-04-26 12:39:49 +0300

    ComtrollersCreated

---
+++ BoardGamesCatalog/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace BoardGamesCatalog.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected string GetUserId()
+        {
+            string userId = string.Empty;
+
+            if (User != null)
+            {
+                userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+
+            return userId;
+        }
+    }
+}
---
+++ BoardGamesCatalog/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using BoardGamesCatalog.NewFolder;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamesCatalog.Controllers
+{
+    public class HomeController : BaseController
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "BoardGames");
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
